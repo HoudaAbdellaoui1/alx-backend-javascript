6fc60047e0f6b43f979990d5238f644ea0592a19
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-01 23:25:50 +0100

    Fix vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const newTask = true;
-    const newTask2 = false;
+    const newTask = false;
+    const newTask2 = true;
     return [newTask, newTask2];
   }
 
