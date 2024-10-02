e3be36549b2e6c027f6f5cff43b40c4aa702e6e2
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-02 19:30:28 +0100

    Use for..of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,6 @@
-Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly.
-
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  for (const elt of array) {
+    elt = appendString + elt;
   }
 
   return array;
