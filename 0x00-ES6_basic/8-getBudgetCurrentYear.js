0b71b521a25bc7e188b5d0a3576b6fc80ee16357
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-02 19:21:51 +0100

    Use computed property

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,13 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {};
-  
-    budget[`income-${getCurrentYear()}`] = income;
-    budget[`gdp-${getCurrentYear()}`] = gdp;
-    budget[`capita-${getCurrentYear()}`] = capita;
-  
-    return budget;
-  }
+  const date = new Date();
+  return date.getFullYear();
+}
 
-//   Rewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+
+  };
+}
