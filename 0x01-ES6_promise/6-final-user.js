719d3b2a90049dbc254551f9a90a6087d7a602b3
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-04 00:00:35 +0100

    Allsettled prom

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values.map((value) => {
+      if (value.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: value.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: value.reason,
+      };
+    }));
+}
