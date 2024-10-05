4a57cdbf712b213b1865084d4a65be8f4c0053a2
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-05 01:22:16 +0100

    Fix creating 3 classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,7 +4,7 @@ export default function initializeRooms() {
   const sizes = [19, 20, 34];
   const result = [];
   for (const s of sizes) {
-    result.push(new ClassRoom(sizes[s]));
+    result.push(new ClassRoom(s));
   }
   return result;
 }
