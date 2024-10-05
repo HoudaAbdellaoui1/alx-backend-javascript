bb3e8d5756b5891f82c513d2701eb75b04d49729
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-05 02:48:04 +0100

    tostring

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+export default class Airport {
+  constructor(name, code) {
+    if ((typeof name) !== 'string') {
+      throw new Error('name must be a string');
+    }
+    this._name = name;
+
+    if ((typeof code) !== 'string') {
+      throw new Error('code must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+//   toString() {
+//     return `Airport [${this.code}] {_name: '${this.name}', _code: '${this.code}'}}`;
+//   }
+
+//   toString() {
+//     return this.toString();
+//   }
+
+  valueOf() {
+    return this._code; // Return the airport code
+  }
+}
