712622688b778b8e0802913e89757d92b84e8951
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-10-08 13:58:18 +0100

    filter by location

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,11 @@
-export default function getListStudentIds(array){
-    if(!Array.isArray(array)){
-        return [];
-    }
-    else {
-        let ids = []
-        array.map((elt) => {
-            ids.push(elt.id);
-          });
-        return ids;
-    }
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+
+  const ids = [];
+  array.forEach((obj) => {
+    ids.push(obj.id);
+  });
+  return ids;
 }
