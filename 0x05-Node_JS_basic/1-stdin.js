c2f26d5753f586cfcd701c6db653d78bc6c4ebe4
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-11-26 23:22:43 +0100

    Exit process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-const readLine = require('readline')
+const readLine = require("readline");
 const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-console.log("Welcome to Holberton School, what is your name?");
-rl.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
-    rl.close();
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('close', () => {
-    console.log("This important software is now closing");
-    process.exit(0)
+console.log("Welcome to Holberton School, what is your name?");
+rl.on("line", (input) => {
+  console.log(`Your name is: ${input}`);
+});
+
+rl.on("close", () => {
+  console.log("This important software is now closing");
+  process.exit(0);
 });
