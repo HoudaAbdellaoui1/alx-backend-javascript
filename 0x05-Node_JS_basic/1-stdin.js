dd620d0946abb530ca77e2090e16c38708429b90
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-11-27 21:42:10 +0100

    update eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read(); // Trim whitespace or newlines from the input
   if(name) {
     process.stdout.write(`Your name is: ${name}`);
@@ -9,6 +9,6 @@ process.stdin.on("readable", () => {
 });
 
 // Handle program exit
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
