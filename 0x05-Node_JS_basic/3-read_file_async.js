51c997bb223dcdf680258acdfd3fbf50fb09c821
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-11-27 22:36:56 +0100

    Fix eslint

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,26 +1,25 @@
 const fs = require('fs').promises;
-function countStudents(path){
-    return fs.readFile(path, 'utf8')
-        .then((data) => {
-            const lines = data.trim().split('\n');
-            const students = lines.slice(1);
-            console.log(`Number of students: ${students.length}`);
-            const fields = {}
-            students.forEach(line => {
-                const [firstname, lastname, age, field] = line.split(',');
-                if(!fields[field])
-                {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);            
-            });
-            for (const [field, names] of Object.entries(fields))
-            {
-                console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-            }
-        })
-        .catch(() => {
-            throw new Error('Cannot load the database');
-        });        
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1);
+      console.log(`Number of students: ${students.length}`);
+      const fields = {};
+      students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
 }
 module.exports = countStudents;
