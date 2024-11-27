ef8dc689604c9f9222380f27c3075611d86d45b0
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-11-27 22:43:54 +0100

    remove listener text

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,8 @@
 const http = require('node:http');
 
 const app = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
-app.listen(1245, () => {
-    console.log('Server is running on port 1245');
-});
+app.listen(1245);
 module.exports = app;
