c17d83a62259931350a7c89e07ead0b2a2cfd8f5
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-11-27 21:35:01 +0100

    update eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 // Listen for user input
 process.stdin.on("readable", () => {
   const name = process.stdin.read(); // Trim whitespace or newlines from the input
-  if (name) {
+  if(name) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
