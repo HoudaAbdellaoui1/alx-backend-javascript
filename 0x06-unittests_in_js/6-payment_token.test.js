92eec45fbdac8ed09782964f3c907375bcd1e3ae
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2025-01-10 00:01:24 +0100

    Using done to resolve

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,8 +2,14 @@ const { expect } = require("chai");
 const getPaymentTokenFromAPI = require("./6-payment_token");
 
 describe("getPaymentTokenFromAPI", () => {
-  it("should return a resolved promise with the correct object when success is true", async () => {
-    const result = await getPaymentTokenFromAPI(true);
-    expect(result).to.deep.equal({ data: "Succssful response from the API" });
+  it("should return a resolved promise with the correct object when success is true", (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(result).to.deep.equal({
+          data: "Succssful response from the API",
+        });
+        done();
+      })
+      .catch((error) => done(error));
   });
 });
